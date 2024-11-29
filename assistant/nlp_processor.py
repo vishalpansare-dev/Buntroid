af14db4366a9a10df70214dd5a70318ded1a1f50
Vishal Pansare <70095753+vishalpansare-dev@users.noreply.github.com>
2024-11-29 12:20:23 +0530

    Update assistant/nlp_processor.py

--- assistant/nlp_processor.py
+++ assistant/nlp_processor.py
@@ -3,6 +3,24 @@ from transformers import pipeline
 # Load NLP model
 nlp_model = pipeline("text-generation", model="EleutherAI/gpt-neo-125M")
 
+# Determine user intent
+intent_model = pipeline("zero-shot-classification")
+
+# List of possible intents
+intents = [
+    "ask_time",
+    "ask_date",
+    "check_email",
+    "create_directory",
+    "delete_directory",
+    "search_file",
+    "generate_text"
+]
+
+def determine_intent(command):
+    result = intent_model(command, intents)
+    return result['labels'][0]
+
 def generate_response(prompt):
     result = nlp_model(prompt, max_length=100, num_return_sequences=1,truncation=True)
     return result[0]['generated_text']
